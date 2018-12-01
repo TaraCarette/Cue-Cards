5d773a807a3f68be7568016a59d97509ed01e59e
tastyfood10 <jasonliu2992@hotmail.com>
2018-12-01 12:36:20 -0500

    use pages.py for making changes to frames

--- createCard.py
+++ createCard.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from tkinter import font  as tkfont
 import os
+import pages
 
 class sampleApp(tk.Tk):
      #this class makes a webpage that will controll which frame we are on
@@ -19,7 +20,7 @@ class sampleApp(tk.Tk):
 
           self.frames = {}
           #i think this makes the other pages
-          for F in (startPage, pageOne, pageTwo):
+          for F in (pages.startPage, pages.pageOne, pages.pageTwo):
                page_name = F.__name__
                frame = F(parent=container, controller=self)
                self.frames[page_name] = frame
@@ -36,46 +37,6 @@ class sampleApp(tk.Tk):
           frame = self.frames[page_name]
           frame.tkraise()
 
-
-class startPage(tk.Frame):
-
-     def __init__(self, parent, controller):
-          tk.Frame.__init__(self, parent)
-          self.controller = controller
-          label = tk.Label(self, text="This is the start page", font=controller.title_font)
-          label.pack(side="top", fill="x", pady=10)
-          #tell the controlling parent to call show_frame with another frame 
-          button1 = tk.Button(self, text="Create",
-                            command=lambda: controller.show_frame("pageOne"), font=(None,15))
-          button2 = tk.Button(self, text="Test",
-                            command=lambda: controller.show_frame("pageTwo"), font=(None,15))
-          button1.pack(side=tk.TOP)
-          button2.pack(side=tk.BOTTOM, pady=10)
-
-
-class pageOne(tk.Frame):
-
-     def __init__(self, parent, controller):
-          tk.Frame.__init__(self, parent)
-          self.controller = controller
-          label = tk.Label(self, text="This is page 1", font=controller.title_font)
-          label.pack(side="top", fill="x", pady=10)
-          #tell the controlling parent to call show_frame with another frame 
-          button = tk.Button(self, text="Go to the start page",
-                           command=lambda: controller.show_frame("startPage"))
-          button.pack()
-
-
-class pageTwo(tk.Frame):
-
-     def __init__(self, parent, controller):
-          tk.Frame.__init__(self, parent)
-          self.controller = controller
-          label = tk.Label(self, text="This is page 2", font=controller.title_font)
-          label.pack(side="top", fill="x", pady=10)
-          button = tk.Button(self, text="Go to the start page",
-                           command=lambda: controller.show_frame("startPage"))
-          button.pack()
         
 class scheduleMenu: #first window
      def __init__(self, parent, controller):
